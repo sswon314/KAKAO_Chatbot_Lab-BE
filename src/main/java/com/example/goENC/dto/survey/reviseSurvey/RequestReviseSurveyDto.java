960ef9043d214dc925ece7a7336ea13bfeacd093
LSW <sswon314@naver.com>
2022-11-05 18:56:36 +0900

    설문 수정 API

---
+++ src/main/java/com/example/goENC/dto/survey/reviseSurvey/RequestReviseSurveyDto.java
@@ -0,0 +1,28 @@
+package com.example.goENC.dto.survey.reviseSurvey;
+
+import com.example.goENC.dto.survey.createSurvey.RequestChoiceAnswerDto;
+import com.example.goENC.dto.survey.createSurvey.RequestQuestionDto;
+import com.example.goENC.models.Survey;
+import com.example.goENC.models.User;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+public class RequestReviseSurveyDto {
+    private User userId = null;
+    private Integer surveyId = null;
+    private String surveyTitle = null;
+    private String surveyContent = null;
+    private List<RequestQuestionDto> questionCardList = null;
+
+    public RequestReviseSurveyDto(Long userId, Integer surveyId, String surveyTitle, String surveyContent, List<RequestQuestionDto> questionCardList) {
+        this.userId = new User(userId);
+        this.surveyId = surveyId;
+        this.surveyTitle = surveyTitle;
+        this.surveyContent = surveyContent;
+        this.questionCardList = questionCardList;
+    }
+}
